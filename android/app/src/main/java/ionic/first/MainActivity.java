a181da85198f866c2a2360ab04926ef6727bec53
Sylwek <drozd.sylwester@o2.pl>
2024-12-08 17:19:06 +0100

    first commit

---
+++ android/app/src/main/java/ionic/first/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.first;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.first',
+  appName: 'ionic-first',
+  webDir: 'dist'
+};
+
+export default config;
